d62ce6151547631e8b6c1282df5547edfb129991
levashov_92 <levashov_92@outlook.com>
2020-12-04 22:02:46 +0300

    Добавьте файлы проекта.

---
+++ SOLID/Startup.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SOLID.Services;
+
+namespace SOLID
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<ICounter, RandomCounter>();
+            services.AddSingleton<CounterService>();
+        }
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMiddleware<CounterMiddleware>();
+        }
+    }
+}
